e07b8c4a6d815f37612babff1103165c4f69207f
Volker Wiegand <volker.wiegand@cvw.de>
2014-08-21 07:42:48 +0200

    removed created_by generator altogether

--- lib/generators/scaffold_plus/created_by/templates/created_by_migration.rb
+++
@@ -1,6 +0,0 @@
-class <%= migration_name.camelize %> < ActiveRecord::Migration
-  def change
-    add_column :<%= table_name %>, :created_by, :integer
-    add_column :<%= table_name %>, :updated_by, :integer
-  end
-end
