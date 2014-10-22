b91fa5cbc266552193367a609d6e19dfe335a956
Volker Wiegand <volker.wiegand@cvw.de>
2014-10-22 20:59:02 +0200

    added geodesic_wgs84 conversion DEG <-> DMS

--- lib/generators/scaffold_plus/geodesic/geodesic_generator.rb
+++ lib/generators/scaffold_plus/geodesic/geodesic_generator.rb
@@ -10,22 +10,35 @@ module ScaffoldPlus
                desc: 'Attribute used for storing latitude'
       class_option :longitude, type: :string, default: "lng",
                desc: 'Attribute used for storing longitude'
-      class_option :before, type: :boolean, default: false,
-               desc: 'Add a line before generated text in model'
-      class_option :after, type: :boolean, default: false,
-               desc: 'Add a line after generated text in model'
 
       def update_model
+        lat = options.latitude
+        lon = options.longitude
         file = "app/models/#{name}.rb"
         prepend_to_file file, "require 'geodesic_wgs84'\n\n"
-        lines = options.before? ? [ "\n" ] : []
-        lines << [
+        lines = [
           "  def to_ary",
-          "    [#{options.latitude}, #{options.longitude}]",
+          "    [#{lat}, #{lon}]",
+          "  end",
+          "",
+          "  protected",
+          "",
+          "  def update_coordinates",
+          "    wgs84 = Wgs84.new",
+          "    if self.#{lat}.blank? and self.#{lon}.blank?",
+          "      if self.#{lat}_dms.present? and self.#{lon}_dms.present?",
+          "        self.#{lat}, self.#{lon} = wgs84.lat_lon(self.#{lat}_dms, self.#{lon}_dms)",
+          "      end",
+          "    end",
+          "    if self.#{lat}_dms.blank? and self.#{lon}_dms.blank?",
+          "      if self.#{lat}.present? and self.#{lon}.present?",
+          "        self.#{lat}_dms, self.#{lon}_dms = wgs84.lat_lon_dms(self.#{lat}, self.#{lon})",
+          "      end",
+          "    end",
+          "    true",
           "  end",
           ""
         ]
-        lines << "\n" if options.after?
         inject_into_class file, class_name, lines.join("\n")
       end
     end
