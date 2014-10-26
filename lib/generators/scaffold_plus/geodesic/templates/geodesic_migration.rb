2e572dbcde97449822ac3cc79dea4f27b68c19e1
Volker Wiegand <volker.wiegand@cvw.de>
2014-10-26 07:29:02 +0100

    added country to address in geodesic generator

--- lib/generators/scaffold_plus/geodesic/templates/geodesic_migration.rb
+++ lib/generators/scaffold_plus/geodesic/templates/geodesic_migration.rb
@@ -4,6 +4,7 @@ class <%= migration_name.camelize %> < ActiveRecord::Migration
     add_column :<%= table_name %>, :<%= options.longitude %>, :float
 <%- if options.address? -%>
     add_column :<%= table_name %>, :address, :string
+    add_column :<%= table_name %>, :country, :string
 <%- end -%>
     add_index :<%= table_name %>, :<%= options.latitude %>
     add_index :<%= table_name %>, :<%= options.longitude %>
